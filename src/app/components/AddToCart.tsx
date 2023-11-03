ec8d6cb24bb8d9b68988930f3a0665abdabd988f
san <spaudyal10@gmail.com>
2023-11-03 10:00:23 +0000

    next js practice

---
+++ src/app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const AddToCart = () => {
+    return (
+        <div onClick={() => console.log('Click')}>AddToCart</div>
+    )
+}
+
+export default AddToCart
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import ProductCart from './components/ProductCart'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <Link href="/users">users</Link>
+      <ProductCart />
+    </main>
+  )
+}
